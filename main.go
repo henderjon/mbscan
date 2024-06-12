ba6d108751a935b031137effc2d38a23c521527d
henderjon <list@box370.net>
2024-06-12 13:56:29 -0500

    adds buildinfo; adds exit codes

--- main.go
+++ main.go
@@ -12,22 +12,36 @@ var (
 )
 
 func init() {
-	flag.BoolVar(&verbose, "v", false, "verbose")
+	bi := getBuildInfo()
+	flag.BoolVar(&verbose, "v", false, "verbose; show any multi-byte characters found in the input stream")
+	flag.Usage = Usage(Info{
+		Bin:            bi.getBinName(),
+		Version:        bi.getBuildVersion(),
+		CompiledBy:     bi.getCompiledBy(),
+		BuildTimestamp: bi.getBuildTimestamp(),
+	})
 	flag.Parse()
 }
 
 func main() {
-	fmt.Println("")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanRunes)
 	bc, rc := 0, 0
 	for scanner.Scan() {
 		if verbose && len(scanner.Bytes()) > 1 {
-			fmt.Printf("rune %d `%s` is %v\n", rc, scanner.Text(), scanner.Bytes())
+			fmt.Fprintf(os.Stderr, "rune %d `%s` is %v\n", rc, scanner.Text(), scanner.Bytes())
 		}
 		bc += len(scanner.Bytes())
 		rc++
 	}
-	fmt.Println("Bytes: ", bc)
-	fmt.Println("Runes: ", rc)
+
+	// fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, " Bytes: ", bc)
+	fmt.Fprintln(os.Stderr, " Runes: ", rc)
+
+	if bc != rc {
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
